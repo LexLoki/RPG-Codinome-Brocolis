ab98e56c56cc1aaa668587fdaf05aa5feb3edbe7
LexLoki <pietroribeiropepe@hotmail.com>
2016-02-09 01:36:35 -0200

    Fix4

--- Bullet/bullet.lua
+++ Bullet/bullet.lua
@@ -17,7 +17,7 @@ Creates a new bullet shot by the given player
 Parameters:
   - x: the x position of the bullet, relative to the top-left corner of the arena
   - y: the y position of the bullet, relative to the top-left corner of the arena
-  - direction: the direction the bullet should go
+  - direction: a table {x = 1 or 0 or -1, y = 1 or 0 or -1}, specifying the direction the bullet should go
 ]]
 function Bullet.new(x,y,direction)
   self = Bullet.newObject(x,y)
