5baef8acbfa2978c2aa2c7e16e8a7cb0345d3219
nikopaes <nickxp51@hotmail.com>
2016-02-24 03:10:07 -0300

    Imagens que eu esqueci no outro Commit, arenaDefault colocada tb, mas não funciona

---
+++ Arena/arenaDefault.lua
@@ -0,0 +1,27 @@
+require "Arena/destructTile"
+arenaComum = {}
+
+local preparePositions, round, evaluate, readTxt
+
+function arenaComum.start(arena)
+  arena.testObstacles()
+end
+function arenaComum.update(dt)
+  
+end
+function arenaComum.destroy(block)
+ 
+end
+function readTxt()
+ 
+end
+function preparePositions(inputs)
+  
+end
+function evaluate(input)
+  
+end
+function round(n)
+  local f = math.floor(n)
+  return n-f < 0.5 and f or f+1
+end
