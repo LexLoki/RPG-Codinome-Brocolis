dedfc7dcc4b2c2c97a995d8cfbf1215267db9f43
nikopaes <nickxp51@hotmail.com>
2016-02-27 02:30:25 -0300

    Modificando ordem da HUd e removendo arquivos desnecessauros

--- Player/playerIcon.lua
+++ Player/playerIcon.lua
@@ -1,6 +1,7 @@
 local playerIcon = {
-  love.graphics.newImage("/Assets/HUD/VR704_HUD_L.png"),
+  
   love.graphics.newImage("/Assets/HUD/Magnolio_HUD_L.png"),
+  love.graphics.newImage("/Assets/HUD/VR704_HUD_L.png"),
   love.graphics.newImage("/Assets/HUD/Godo_HUD_2L.png"),
   love.graphics.newImage("/Assets/HUD/Capita_Rubi_HUD_L.png")
 
